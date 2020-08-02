52dc533fca975346cdef14654788ae51ccab9f36
michmich6 <michmich6@users.noreply.github.com>
2020-08-02 12:27:54 -0600

    First commit

---
+++ src/build/precache-manifest.a004e16223a64c492f0e8109322ef90c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b970d680b2da80a143a5039059c73c59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "201523d5443e21fdcc81",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "c625653371890179d1e7",
+    "url": "/static/js/2.eb729104.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb729104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201523d5443e21fdcc81",
+    "url": "/static/js/main.8627ee39.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
